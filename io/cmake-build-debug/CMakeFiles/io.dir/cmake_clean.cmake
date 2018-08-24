7ea38c33c18688ea8170fa8d25f9a5a6128caed7
zang_song <zang_song@gowild.cn>
2018-08-24 17:53:39 +0800

    io api

---
+++ io/cmake-build-debug/CMakeFiles/io.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/io.dir/main.c.o"
+  "io.pdb"
+  "io"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/io.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
