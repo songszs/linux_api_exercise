1cb359c84856d6c92f9e21733160b7e421600d6a
zang_song <zang_song@gowild.cn>
2018-08-24 19:01:39 +0800

    文件读写

---
+++ linux_api/cmake-build-debug/CMakeFiles/linux_api.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linux_api.dir/file_api/copy.c.o"
+  "CMakeFiles/linux_api.dir/lib/error_functions.c.o"
+  "CMakeFiles/linux_api.dir/lib/get_num.c.o"
+  "CMakeFiles/linux_api.dir/lib/alt_functions.c.o"
+  "CMakeFiles/linux_api.dir/lib/syscall_speed.c.o"
+  "linux_api.pdb"
+  "linux_api"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/linux_api.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
